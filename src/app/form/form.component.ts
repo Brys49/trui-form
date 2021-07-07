b044a2aa24cb5113d369d6c668b81b697daa1358
Krzysztof Bryszak <krzysztof4499@gmail.com>
2021-07-07 10:25:49 +0200

    form component created

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.sass']
+})
+export class FormComponent implements OnInit {
+  formGroup!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  get name() {
+    return this.formGroup.get('name') as FormControl;
+  }
+
+  createForm() {
+    this.formGroup = this.formBuilder.group({
+      'name': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'nip': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'email': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'phone': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'driving-licence': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'subject': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'textarea': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+    });
+  }
+
+  onSubmit(post: any) {
+    console.log(post);
+  }
+
+}
