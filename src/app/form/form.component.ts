2d5cec231f16d3ceb4f5b066f553a797c1a46fb9
Krzysztof Bryszak <krzysztof4499@gmail.com>
2021-07-09 01:04:26 +0200

    working form

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, ValidatorFn, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -9,8 +9,7 @@ import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms'
 export class FormComponent implements OnInit {
   formGroup!: FormGroup;
   floatLabelControl = new FormControl('always');
-
-  checkedControl = new FormControl(false);
+  allChecked: boolean = false;
 
   constructor(
     private formBuilder: FormBuilder
@@ -18,29 +17,63 @@ export class FormComponent implements OnInit {
 
   ngOnInit(): void {
     this.createForm();
-  }
-
-  get name() {
-    return this.formGroup.get('name') as FormControl;
+    this.formGroup.get('option-1')?.valueChanges.subscribe(next => { this.allChecked = next && this.formGroup.get('option-2')?.value });
+    this.formGroup.get('option-2')?.valueChanges.subscribe(next => { this.allChecked = next && this.formGroup.get('option-1')?.value });
   }
 
   createForm() {
     this.formGroup = this.formBuilder.group({
-      'name': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
-      'nip': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
-      'email': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
-      'phone': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
-      'driving-licence': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
-      'subject': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
-      'textarea': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(30)]],
+      'name': [null, [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
+      'nip': [null, [Validators.required, nipValidator()]],
+      'email': [null, [Validators.required, Validators.email]],
+      'phone': [null, [Validators.pattern("[0-9]{3} [0-9]{3} [0-9]{3}")]],
+      'subject': [null, [Validators.required]],
+      'driving-licence': ["false", [Validators.required]],
+      'textarea': [null, [Validators.minLength(1), Validators.maxLength(300)]],
+      'option-1': [false, [Validators.requiredTrue]],
+      'option-2': [false],
       floatLabel: this.floatLabelControl,
-      checked: this.checkedControl,
     });
   }
 
   onSubmit(post: any) {
     console.log(post);
   }
-  
 
+  checkAll() {
+    this.allChecked = !this.allChecked;
+
+    if (this.allChecked == true) {
+      this.formGroup.get('option-1')?.setValue(true);
+      this.formGroup.get('option-2')?.setValue(true);
+    } else {
+      this.formGroup.get('option-1')?.setValue(false);
+      this.formGroup.get('option-2')?.setValue(false);
+    }
+  }
+
+}
+
+export function nipValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const isValid = isValidNip(control.value);
+    return isValid ? {nip: {value: control.value}} : null;
+  };
+}
+
+function isValidNip(nip: any) {
+  if(typeof nip !== 'string')
+        return false;
+
+    nip = nip.replace("/[\ \-]/gi", '');
+
+    let weight = [6, 5, 7, 2, 3, 4, 5, 6, 7];
+    let sum = 0;
+    let controlNumber = parseInt(nip.substring(9, 10));
+    let weightCount = weight.length;
+    for (let i = 0; i < weightCount; i++) {
+        sum += (parseInt(nip.substr(i, 1)) * weight[i]);
+    }
+    
+    return sum % 11 === controlNumber;
 }
