7027622b37a975c3498d20f043e51ce29ee30dce
Krzysztof Bryszak <krzysztof4499@gmail.com>
2021-07-09 17:52:06 +0200

    removed reduntant imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,8 +8,7 @@ import { FormComponent } from './form/form.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
 
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 
 @NgModule({
   declarations: [
@@ -22,8 +21,6 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     ReactiveFormsModule,
     NoopAnimationsModule,
     MaterialModule,
-    MatInputModule,
-    MatFormFieldModule,
     BrowserAnimationsModule
   ],
   providers: [],
